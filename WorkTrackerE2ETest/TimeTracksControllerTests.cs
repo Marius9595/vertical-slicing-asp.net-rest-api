c2148fe462c9807109d0ea989b8e8dd7b3d98fc5
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2024-02-15 10:39:42 +0000

    :tada: Created E2E test proyect

---
+++ WorkTrackerE2ETest/TimeTracksControllerTests.cs
@@ -0,0 +1,15 @@
+namespace WorkTrackerE2ETest;
+
+public class TimeTracksControllerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Foo()
+    {
+        
+    }
+}
